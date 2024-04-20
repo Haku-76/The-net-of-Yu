45edf6670f4c830913dc8e2ebaecdf52b8e74094
Haku_76 <baiang.li@outlook.com>
2024-04-20 22:40:04 +0900

    240420

---
+++ Assets/Scripts/VolumeSlider.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class VolumeSlider : MonoBehaviour
+{
+    //public void SetBGMVolume(float volume)
+    //{
+    //    SoundManager.Instance.SetBGMVolume(volume);
+    //}
+
+    //public void SetSEVolume(float volume)
+    //{
+    //    SoundManager.Instance.SetSEVolume(volume);
+    //}
+}
